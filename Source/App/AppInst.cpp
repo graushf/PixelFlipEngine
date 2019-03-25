5f61c37c8955dccb7658a82dd19e2327ac215a9b
Graushf <graushf@gmail.com>
2019-03-26 00:34:45 +0100

    Moving AppInst.cpp to correct folder

--- Source/App/AppInst.cpp
+++ Source/App/AppInst.cpp
@@ -1,50 +1,5 @@
-#define WIN32_LEAN_AND_MEAN
 
-// Windows Header Files:
-
-#define NOMINMAX
-#include <windows.h>
-#include <windowsx.h>
-
-#include <crtdbg.h>
-
-// C Runtime Header Files
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
-
-#include <mmsystem.h>
-
-#include <algorithm>
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <map>
-
-#if _MSC_VER >= 1700
-#include <memory>
-	using std::shared_ptr;
-	using std::weak_ptr;
-	using std::static_pointer_cast;
-	using std::dynamic_pointer_cast;
-#elif
-	using std::tr1::shared_ptr;
-	using std::tr1::weak_ptr;
-	using std::tr1::static_pointer_cast;
-	using std::tr1::dynamic_pointer_cast;
-#endif
-
-#define DXUT_AUTOLIB
-
-// DirectX Includes
-#include <dxut.h>
-#include <d3dx9tex.h>
-#include <dxut.h>
-#include <SDKmisc.h>
-
-
+#include "Common/CommonStd.h"
 
 // -------------------------------------------------------------------------------------
 // Entry point to the program. Initializes everything and goes into a message processing
@@ -53,10 +8,13 @@
 // See Game Coding Complete 4th Edition - Chapter 5, page 134
 // -------------------------------------------------------------------------------------
 
-INT WINAPI AppInst(HINSTANCE hInstance,
-					  HINSTANCE hPrevInstance,
-					  LPWSTR lpCmdLine,
-					  int nCmdShow)
+INT WINAPI AppInst
+	(
+		HINSTANCE hInstance, 
+		HINSTANCE hPrevInstance, 
+		LPWSTR lpCmdLine, 
+		int nCmdShow
+	)
 {
 	// Set up checks for memory leaks.
 	// Game Coding Complete reference - Chapter 21, page 834
@@ -77,7 +35,7 @@ INT WINAPI AppInst(HINSTANCE hInstance,
 	_CrtSetDbgFlag(tmpDbgFlag);
 
 	// [rez] Initialize the logging system as the very first thing you ever do!
-	Logger::Init("logging.xml");
+	//* Logger::Init("logging.xml");
 
 	g_pApp->m_Options.Init("PlayerOptions.xml", lpCmdLine);
 	
